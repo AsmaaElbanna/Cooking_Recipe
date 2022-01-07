1a7f9d106445b44832221b499f5eb2158643ee3d
asmaa elbanna <asmaaelbanna21@gmail.com>
2022-01-07 15:42:10 +0200

    recipe card

--- lib/screens/home/components/recipe_today_card.dart
+++ lib/screens/home/components/recipe_today_card.dart
@@ -1,10 +1,87 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 class RecipeTodayCard extends StatelessWidget {
   const RecipeTodayCard({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: EdgeInsets.all(10),
+      width: 200,
+
+      decoration: BoxDecoration(
+        color: Colors.grey.withOpacity(0.2),
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Icon(Icons.favorite_border),
+          Image.asset('asset/images/1.png'),
+          Text(
+            'Breakfast',
+            style: TextStyle(color: Colors.blueAccent),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text('French Toast with Berries'),
+          SizedBox(
+            height: 10,
+          ),
+          RatingBar.builder(
+            initialRating: 3.5,
+            minRating: 1,
+            direction: Axis.horizontal,
+            allowHalfRating: true,
+            itemCount: 5, itemSize: 20,
+            // itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
+            itemBuilder: (context, _) => const Icon(
+              Icons.star,
+              color: Colors.deepOrange,
+            ),
+            onRatingUpdate: (rating) {
+              print(rating);
+            },
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          const Text(
+            '120 Calories',
+            style: TextStyle(color: Colors.deepOrange),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Icon(
+                    Icons.access_time,
+                    color: Colors.grey,
+                  ),
+                  Text(
+                    '10 mins',
+                    style: TextStyle(color: Colors.grey),
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Icon(Icons.room_service_outlined, color: Colors.grey),
+                  Text('1 Serving', style: TextStyle(color: Colors.grey))
+                ],
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cooking_recipe_app/screens/home/components/recipe_today_card.dart';
 import 'package:cooking_recipe_app/screens/home/components/search.dart';
 import 'package:flutter/material.dart';
 
@@ -8,59 +9,71 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              const Icon(
-                Icons.segment,
-                size: 30,
-              ),
-              Image.asset(
-                'asset/icons/bell.png',
-                height: 30,
-                width: 30,
-              )
-            ],
-          ),
-          const SizedBox(height: 20),
-          const Text(
-            'Bonjour, Asmaa',
-            style: TextStyle(color: Colors.grey),
-          ),
-          const SizedBox(height: 10),
-          const Text(
-            'What Would you like to cooke today?',
-            style: TextStyle(color: Colors.white, fontSize: 20),
-          ),
-          const SizedBox(height: 20),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: const [
-              SearchBar(),
-              Icon(
-                Icons.filter_alt_outlined,
-                size: 30,
-              ),
-            ],
-          ),
-          const SizedBox(height: 20),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              const Text(
-                'Today\'s Fresh Recipes',
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold),
-              ),
-              TextButton(onPressed: () {}, child: const Text('See All'))
-            ],
-          )
-        ],
+      child: Padding(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Icon(
+                  Icons.segment,
+                  size: 30,
+                ),
+                Image.asset(
+                  'asset/icons/bell.png',
+                  height: 30,
+                  width: 30,
+                )
+              ],
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Bonjour, Asmaa',
+              style: TextStyle(color: Colors.grey),
+            ),
+            const SizedBox(height: 10),
+            const Text(
+              'What Would you like to cooke today?',
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const [
+                SearchBar(),
+                Icon(
+                  Icons.filter_alt_outlined,
+                  size: 30,
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  'Today\'s Fresh Recipes',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                ),
+                TextButton(onPressed: () {}, child: const Text('See All',style: TextStyle(color: Colors.deepOrange),))
+              ],
+            ),
+            // Container(
+            //   child: ListView.builder(
+            //       scrollDirection: Axis.horizontal,
+            //       itemCount: 3,itemBuilder: (context,index){
+            //     return Container(
+            //       child: RecipeTodayCard(),
+            //     );
+            //   }),
+            // )
+          ],
+        ),
       ),
     ));
   }
